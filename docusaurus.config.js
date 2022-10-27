8a9c59698bcbd7302c3b2f347c65190e4f1caf0f
wshzard <moriafly@163.com>
2022-10-27 15:41:07 +0800

    Url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'WORLDSCI',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://moriafly.xyz',
+  baseUrl: '/HiMoriafly/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
